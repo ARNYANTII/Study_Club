07e5f15e8d8ae27b1f2309ad08a16edced287870
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-11-14 06:21:35 -0800

    menghitung gaji karyawan tanpa mengunakan inputan

---
+++ day35.java
@@ -0,0 +1,11 @@
+package ISC;
+public class day35 {
+    public static void main(String[] args) {
+        double gajiPokok = 2000000;  
+        double tunjangan = 500000;
+        double totalGaji = gajiPokok + tunjangan;
+        System.out.println("Gaji Pokok: " + gajiPokok);
+        System.out.println("Tunjangan: " + tunjangan);
+        System.out.println("Total Gaji: " + totalGaji);
+    }
+}
