0afb28d2f2d9584525f9f7e9fa7014d93af4ded5
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-11-16 15:22:34 +0800

    update nilai variabel dengan tipe data integer

---
+++ Day38.java
@@ -0,0 +1,12 @@
+package ISC;
+public class Day38 {
+    public static void main(String[] args) {
+        int angka1 = 20;
+        int angka2 = 100;
+        angka1 = 50;
+        angka2 = 150;
+        System.out.println("Nama Depan :"+angka1);
+        System.out.println("Nama Belakang :"+angka2);
+    }
+}
+
