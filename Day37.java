53235b4a3994e357f68fa28e5bc0d01885c23fa3
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-11-16 15:19:21 +0800

    Update variabel dengan tipe data String

---
+++ Day37.java
@@ -0,0 +1,11 @@
+package ISC;
+public class Day37 {
+    public static void main(String[] args) {
+        String NamaDepan = "dika";
+        String NamaBelakang = "cika";
+        NamaDepan = "disa";
+        NamaBelakang = "citra";
+        System.out.println("Nama Depan :"+NamaDepan);
+        System.out.println("Nama Belakang :"+NamaBelakang);
+    }
+}
