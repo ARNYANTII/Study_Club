c9f52d2db07de2d776eff04a92ede82d2fbd6458
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-11-16 15:28:48 +0800

    Update nilai variabel dengan tipe data double

---
+++ Day39.java
@@ -0,0 +1,11 @@
+package ISC;
+public class Day39 {
+     public static void main(String[] args) {
+        double angka1 = 3.14;
+        double angka2 = 2.5;
+        angka1 = 5.20;
+        angka2 = 15.3;
+        System.out.println("Nama Depan :"+angka1);
+        System.out.println("Nama Belakang :"+angka2);
+    }
+}
