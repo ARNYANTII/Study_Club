f8e3de2f95b05a25d1c49deb82947f19bff87f63
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-11-16 23:41:50 +0800

    menghitung nilai variabel mengunakan kondisi
    jika hasil penjumlahannya ganjil maka di tambah 1 dan jika hasil penjumlahannya genap maka di tambah dua

---
+++ Day40.java
@@ -0,0 +1,18 @@
+package ISC;
+import java.util.Scanner;
+public class Day40 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("masukkan angkah pertama :");
+        int angka1 = input.nextInt();
+        System.out.print("masukkan angka ke dua :");
+        int angka2 = input.nextInt();
+        System.out.print("masukkan angka ketiga :");
+        int angka3 = input.nextInt();
+        int hasil = angka1+angka2+angka3;
+        if (hasil % 2  !=0){
+            hasil +=1;
+        }
+        System.out.println("hasil penjumlahan :"+hasil);
+    }
+}
