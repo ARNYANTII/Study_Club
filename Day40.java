e98a697772c0636b7cf5f5621a8b4115f1333097
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-11-18 06:15:30 +0800

    menghitung_nilai_variabel

--- Day40.java
+++ Day40.java
@@ -1,7 +1,7 @@
 package ISC;
 import java.util.Scanner;
 public class Day40 {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
         System.out.print("masukkan angkah pertama :");
         int angka1 = input.nextInt();
@@ -10,9 +10,13 @@ public class Day40 {
         System.out.print("masukkan angka ketiga :");
         int angka3 = input.nextInt();
         int hasil = angka1+angka2+angka3;
-        if (hasil % 2  !=0){
+        if (hasil % 2  ==0){
+            hasil +=2;
+            System.out.println("hasil penjumlahan genap maka di tambah 2 = "+hasil);
+        }else if (hasil % 2 ==1){
             hasil +=1;
+            System.out.println("hasil penjumlahan ganjil maka di tambah 1 = "+hasil);
         }
-        System.out.println("hasil penjumlahan :"+hasil);
+        
     }
 }
