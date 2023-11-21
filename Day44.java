7fbcef25bae263b052385c2f4a14796953075ce1
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-11-21 22:29:42 +0800

    menghitung jumlah bilangan genap antara nilai awal dan akhir.

---
+++ Day44.java
@@ -0,0 +1,19 @@
+package ISC;
+import java.util.Scanner;
+public class day44 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("masukkan nilai awal : ");
+        int awal = input.nextInt();
+        System.out.print("masukkan nilai akhir : ");
+        int akhir = input.nextInt();
+        int totalgenap = 0;
+        for (int i = awal; i <= akhir; i++) {
+            if (i % 2 == 0){
+               totalgenap += i; 
+            }
+        }
+        System.out.println("jumlah total bilangan Genap adalah : "+totalgenap);
+    }
+}
+   
