f344fdd48699b55046b7bc9880a76e39af41c6c1
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-11-22 21:23:27 +0800

    Menampilkan nilai dari variabel hingga 30 secara berurutan

---
+++ Day45.java
@@ -0,0 +1,13 @@
+package ISC;
+import java.util.Scanner;
+public class day45 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("masukkan nilai : ");
+        int nilai = input.nextInt();
+        for (int i = nilai; i <= 30; i++) {
+            System.out.println("angka "+ i);
+            
+        }
+    }
+}
