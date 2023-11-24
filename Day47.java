c73e1cc3b47e28fb6b2c8a0813a3e904ce08d2b8
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-11-24 23:38:05 +0800

    membuat Array mengunakan tipe data Integer

---
+++ Day47.java
@@ -0,0 +1,14 @@
+package ISC;
+public class Day47 {
+    public static void main(String[] args) {
+        int array[]= {23,45,13,16,21,67,29};
+            System.out.println("array ke-1: " +array[0]);
+            System.out.println("array ke-1: " +array[1]);
+            System.out.println("array ke-1: " +array[2]);
+            System.out.println("array ke-1: " +array[3]);
+            System.out.println("array ke-1: " +array[4]);
+            System.out.println("array ke-1: " +array[5]);
+            System.out.println("array ke-1: " +array[6]);
+        
+    }
+}
