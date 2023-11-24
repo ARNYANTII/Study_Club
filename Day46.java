714d83e417a1b1f6ee2bde595b703dcfcd391ab8
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-11-24 23:30:22 +0800

    update nilai variabel dengan tipe data boolean

---
+++ Day46.java
@@ -0,0 +1,13 @@
+package ISC;
+public class Day46 {
+    public static void main(String[] args) {
+        boolean nilai1 = true;
+        boolean nilai2 = false;
+        nilai1 = false;
+        nilai2 = true;
+        System.out.println("nilai dari nilai1 : " +nilai1);
+        System.out.println("nilai dari nilai2 : "+nilai2);
+        
+    }
+    
+}
