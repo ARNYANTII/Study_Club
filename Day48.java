bb4f31419582d5d6c76c64bbb2b76163476c30b3
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-11-26 22:21:22 +0800

    menampilkan DaftarBuah mengunakan ArrayList

---
+++ Day48.java
@@ -0,0 +1,13 @@
+package ISC;
+import java.util.ArrayList;
+public class day48 {
+    public static void main(String[] args) {
+        ArrayList <String>  daftarbuah = new ArrayList<>();
+        daftarbuah.add("mangga");
+        daftarbuah.add("apel");
+        daftarbuah.add("nanas");
+        daftarbuah.add("pepaya");
+        
+        System.out.println("daftar nama buah : " +daftarbuah );
+    }
+}
