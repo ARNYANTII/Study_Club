8f4cc896a5e9fd09971d20c75f838e9e0ea722f7
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-11-29 23:09:27 +0800

    menjumlahkan setiap angka yang di inputkan

---
+++ Day50.java
@@ -0,0 +1,18 @@
+package ISC;
+import java.util.Scanner;
+public class Day50 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int total = 0;
+        while (true) {
+            System.out.print("masukan angka :");
+            int angka = input.nextInt();
+            if (angka == 0) {
+                break;
+            }
+            total += angka;
+            System.out.println("hasil :" + total);
+
+        }
+    }
+}
