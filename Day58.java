124ba717bfa251932b22936b6b6beb67821e7c9c
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-12-06 22:48:50 +0800

    ArrayList_Daftar Buah

---
+++ Day58.java
@@ -0,0 +1,15 @@
+package ISC;
+import java.util.ArrayList;
+public class Day58 {
+    public static void main(String[] args) {
+        ArrayList<String> ListBuah = new ArrayList<>();
+        ListBuah.add("semangkah");
+        ListBuah.add("melon");
+        ListBuah.add("jeruk");
+        ListBuah.add("apel");
+
+        for (String DaftarBuah : ListBuah) {
+            System.out.println("Daftar buah : " + DaftarBuah);
+        }
+    }
+}
