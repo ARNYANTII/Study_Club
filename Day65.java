3ec0f57c19026b12c3065dec6a7022e8bd5e49b9
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-12-12 22:01:34 +0800

    menampilkan jumlah elemen dalam ArrayList

---
+++ Day65.java
@@ -0,0 +1,14 @@
+package ISC;
+import java.util.LinkedList;
+public class Day65 {
+        public static void main(String[] args) {
+    LinkedList <String> namaList = new LinkedList <>();
+    namaList.add("arni");
+    namaList.add("ani");
+    namaList.add("yanti");
+    namaList.add("nia");
+    namaList.add("anti");
+    //menampilkan jumlah elemen dalam array
+        System.out.println("menampilkan jumlah elemen dalam array :"+namaList.size()); 
+    }
+}
