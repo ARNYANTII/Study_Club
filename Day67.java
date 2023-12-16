a171564a22954d06c7138a8580533923c465622d
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2023-12-16 15:26:22 +0800

    Memeriksa ArrayList Kosong atau Tidak

---
+++ Day67.java
@@ -0,0 +1,14 @@
+package ISC;
+import java.util.ArrayList;
+public class Day67 {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        
+        if (names.isEmpty()) {
+            System.out.println("ArrayList kosong.");
+        } else {
+            System.out.println("ArrayList tidak kosong.");
+        }
+    }
+}
+
