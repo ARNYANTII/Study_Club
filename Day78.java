721e322dd6454030640811261e2f444c37f7ad29
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2024-01-03 13:44:25 +0800

    Array untuk Menyimpan Nilai Ujian

---
+++ Day78.java
@@ -0,0 +1,11 @@
+package ISC;
+public class Day78 {
+    public static void main(String[] args) {
+        double[] nilaiUjian = {85.5, 78.0, 92.5};
+
+        System.out.println("Nilai Ujian:");
+        for (double nilai : nilaiUjian) {
+            System.out.println(nilai);
+        }
+    }
+}
