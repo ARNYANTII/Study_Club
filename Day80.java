8417e5aa6707b370ed2f1649d24ce382c42e7546
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2024-01-03 13:47:36 +0800

    Array untuk Menyimpan Nama Mahasiswa dan Nilai Ujian

---
+++ Day80.java
@@ -0,0 +1,12 @@
+package ISC;
+public class Day80 {
+    public static void main(String[] args) {
+        String[] namaSiswa = {"arni", "yanti", "tia"};
+        int[] nilaiUjian = {85, 78, 92};
+
+        System.out.println("Nilai Siswa:");
+        for (int i = 0; i < namaSiswa.length; i++) {
+            System.out.println(namaSiswa[i] + ": " + nilaiUjian[i]);
+        }
+    }
+}
