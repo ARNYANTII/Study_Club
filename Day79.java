99a2b55ccf86670a37e02b0b7594b969bb6e29e8
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2024-01-03 13:45:23 +0800

    Array untuk Menyimpan Koordinat Titik 2D

---
+++ Day79.java
@@ -0,0 +1,12 @@
+package ISC;
+public class Day79 {
+    public static void main(String[] args) {
+        double[] koordinatX = {2.0, -1.5, 3.0};
+        double[] koordinatY = {1.0, 4.5, -2.0};
+      
+        System.out.println("Titik 2D:");
+        for (int i = 0; i < koordinatX.length; i++) {
+            System.out.println("(" + koordinatX[i] + ", " + koordinatY[i] + ")");
+        }
+    }
+}
