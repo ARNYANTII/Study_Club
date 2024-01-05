dae9c8375ce3d422845edf6675f1e951b23f2015
ARNYANTII <135333808+ARNYANTII@users.noreply.github.com>
2024-01-05 14:35:33 +0800

    Mengubah Elemen Array Menjadi String

---
+++ Day90.java
@@ -0,0 +1,8 @@
+package ISC;
+public class Day90 {
+    public static void main(String[] args) {
+        char[] array = {'A', 'R', 'N', 'Y', 'A', 'N', 'T', 'I'};
+        String kalimat = new String(array);
+        System.out.println("Hasil konversi array ke string: " + kalimat);
+    }
+}
